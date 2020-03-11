04236936141e24e2717d9f5f3c2b8750b91b70ef
lyeeteng1999 <lyeeteng1999@gmail.com>
2020-03-11 15:36:27 +0800

    change change

--- Neutral/model.py
+++ Neutral/model.py
@@ -17,7 +17,13 @@ class User(db.Model): #User info
     weight = db.Column(db.Float, nullable = False)
     image = db.Column(db.String(20), nullable = False, default = 'default.jpg')
     healthGoal = db.Column(db.String(120), nullable = False, default = 'Lose 0.5kg in a week')
-    
+    FoodRecord = db.relationship("FoodRecord", cascade="all,delete", backref="User")
+    Food = db.relationship("Food", cascade="all,delete", backref="User")
+    WorkoutRecord = db.relationship("WorkoutRecord", cascade="all,delete", backref="User")
+    Workout = db.relationship("Workout", cascade="all,delete", backref="User")
+    Exercise = db.relationship("Exercise", cascade="all,delete", backref="User")
+    Set = db.relationship("Set", cascade="all,delete", backref="User")
+    Rep = db.relationship("Rep", cascade="all,delete", backref="User")
 
 class FoodDB(db.Model): #from food API
     __tablename__ = 'FoodDB'
@@ -27,8 +33,11 @@ class FoodDB(db.Model): #from food API
     food_protein = db.Column(db.Float, nullable = False)
     food_carb = db.Column(db.Float, nullable = False)
     food_fat = db.Column(db.Float, nullable = False)
+    food_fibres = db.Column(db.Float, nullable = False)
+    food_saturatedfat = db.Column(db.Float, nullable = False)
     food_sodium = db.Column(db.Float, nullable = False)
     serving_size = db.Column(db.Float, nullable = False)
+    Food = db.relationship("Food", cascade="all,delete", backref="FoodDB")
 
 class FoodRecord(db.Model): #a food record that user eat
     __tablename__ = 'FoodRecord'
@@ -36,36 +45,41 @@ class FoodRecord(db.Model): #a food record that user eat
     user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'), nullable = False)
     foodrecord_date = db.Column(db.DateTime, nullable = False, default = datetime.now())
     foodrecord_meal = db.Column(db.String(20), nullable = False, default = 'Breakfast')
-    user = db.relationship("User")
+    Food = db.relationship("Food", cascade="all,delete", backref="FoodRecord")
 
 class Food(db.Model): #individual food eaten by user
     __tablename__ = 'Food'
-    food_id = db.Column(db.Integer, db.ForeignKey('FoodDB.food_id'), nullable = False, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'), nullable = False, primary_key=True)
-    foodrecord_id = db.Column(db.Integer, db.ForeignKey('FoodRecord.foodrecord_id'), nullable = False, primary_key=True)
+    food_id = db.Column(db.Integer, db.ForeignKey('FoodDB.food_id'), nullable = False, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'), nullable = False, primary_key = True)
+    foodrecord_id = db.Column(db.Integer, db.ForeignKey('FoodRecord.foodrecord_id'), nullable = False, primary_key = True)
     food_name = db.Column(db.Float, nullable = False)
     food_calories = db.Column(db.Float, nullable = False)
     food_protein = db.Column(db.Float, nullable = False)
     food_carb = db.Column(db.Float, nullable = False)
     food_fat = db.Column(db.Float, nullable = False)
+    food_fibres = db.Column(db.Float, nullable = False)
+    food_saturatedfat = db.Column(db.Float, nullable = False)
     food_sodium = db.Column(db.Float, nullable = False)
     serving_size = db.Column(db.Float, nullable = False)
-    fooddb = db.relationship("FoodDB")
-    user = db.relationship("User")
-    foodrecord = db.relationship("FoodRecord")
 
 class ExerciseDB(db.Model): # from exercise API
     __tablename__ = 'ExerciseDB'
     exercise_id = db.Column(db.Integer, primary_key = True)
     exercise_desc = db.Column(db.String(1000000), nullable = False)
     exercise_caloriesburnt = db.Column(db.Float, nullable = False)
+    Exercise = db.relationship("Exercise", cascade="all,delete", backref="ExerciseDB")
+    Set = db.relationship("Set", cascade="all,delete", backref="ExerciseDB")
+    Rep = db.relationship("Rep", cascade="all,delete", backref="ExerciseDB")
 
 class WorkoutRecord(db.Model):#workout that user is supposed to do that day
     __tablename__ = 'WorkoutRecord'
     workoutrecord_id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'), nullable = False)
     workoutrecord_date = db.Column(db.DateTime, nullable = False, default = datetime.now())
-    User = db.relationship("User")
+    Workout = db.relationship("Workout", cascade="all,delete", backref="WorkoutRecord")
+    Exercise = db.relationship("Exercise", cascade="all,delete", backref="WorkoutRecord")
+    Set = db.relationship("Set", cascade="all,delete", backref="WorkoutRecord")
+    Rep = db.relationship("Rep", cascade="all,delete", backref="WorkoutRecord")
 
 class Workout(db.Model):#one workout of user
     __tablename__ = 'Workout'
@@ -73,8 +87,9 @@ class Workout(db.Model):#one workout of user
     workoutrecord_id = db.Column(db.Integer, db.ForeignKey('WorkoutRecord.workoutrecord_id'), nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'), nullable = False)
     workout_name = db.Column(db.String(50), nullable = False)
-    workoutrecord = db.relationship("WorkoutRecord")
-    user = db.relationship("User")
+    Exercise = db.relationship("Exercise", cascade="all,delete", backref="Workout")
+    Set = db.relationship("Set", cascade="all,delete", backref="Workout")
+    Rep = db.relationship("Rep", cascade="all,delete", backref="Workout")
 
 class Exercise(db.Model):#one exercise in workout
     __tablename__ = 'Exercise'
@@ -85,10 +100,6 @@ class Exercise(db.Model):#one exercise in workout
     exercise_desc = db.Column(db.String(1000000), nullable = False)
     exercise_caloriesburnt = db.Column(db.Float, nullable = False)
     status = db.Column(db.Boolean, nullable = False, default = False)
-    exercisedb = db.relationship("ExerciseDB")
-    workout = db.relationship("Workout")
-    workoutrecord = db.relationship("WorkoutRecord")
-    user = db.relationship("User")
 
 class Set(db.Model):#one set in exercise
     __tablename__ = 'Set'
@@ -98,10 +109,8 @@ class Set(db.Model):#one set in exercise
     workoutrecord_id = db.Column(db.Integer, db.ForeignKey('WorkoutRecord.workoutrecord_id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'), nullable = False)
     set_count = db.Column(db.Integer, nullable = False)
-    exercisedb = db.relationship("ExerciseDB")
-    workout = db.relationship("Workout")
-    workoutrecord = db.relationship("WorkoutRecord")
-    user = db.relationship("User")
+    Rep = db.relationship("Rep", cascade="all,delete", backref="Set")
+
 
 class Rep(db.Model):#one in set
     __tablename__ = 'Rep'
@@ -114,8 +123,4 @@ class Rep(db.Model):#one in set
     rep_count = db.Column(db.Integer, nullable = False)
     rep_unit = db.Column(db.String(20), nullable = False)#e.g. 50 kg, kg is the desc
     rep_desc = db.Column(db.String(20), nullable = False) #e.g. 50kg, 50 is the desc
-    Set = db.relationship("Set")
-    exercisedb = db.relationship("ExerciseDB")
-    workout = db.relationship("Workout")
-    workoutrecord = db.relationship("WorkoutRecord")
-    user = db.relationship("User")
+
