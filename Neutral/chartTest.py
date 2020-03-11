f0c0e76dd56769b64546164f77b73c0f10e56dd2
espressodepresso <56994283+espressodepresso@users.noreply.github.com>
2020-03-11 13:02:10 +0800

    charts & tables

--- Neutral/chartTest.py
+++ Neutral/chartTest.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def chartDisplay():
     NutritionData =[30,30,40]
+    foodinfo=[{'fname':'Avocado Toast','calories':244,'carbs':27,'fats':18.3}]
 
-    return render_template('charts.html', values=NutritionData, tableData=)
+
+    return render_template('charts.html', values=NutritionData, tableData=foodinfo)
 
 if __name__ == '__main__':
     app.run(debug=True)
