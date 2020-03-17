ded909fb624de5c188477c60cdd636eb9db648b5
lyeeteng1999 <lyeeteng1999@gmail.com>
2020-03-17 17:23:07 +0800

    EXERCISE DB UP

--- Neutral/importapi.py
+++ Neutral/importapi.py
@@ -3,22 +3,19 @@ import sqlalchemy
 
 engine = sqlalchemy.create_engine('sqlite:///site.db')
 
-df = pd.read_csv('allFoodInfo.csv', encoding= 'unicode_escape')
-df['serving_size'] = 1
+df = pd.read_csv('allExerciseInfo.csv', encoding= 'unicode_escape')
 
 df.rename(columns={
-    'name' : 'food_name',
-    'calorie' : 'food_calories',
-    'protein' : 'food_protein',
-    'carb' : 'food_carb',
-    'fat' : 'food_fat',
-    'fat_saturated' : 'food_saturatedfat',
-    'fibres' : 'food_fibres',
-    'sodium' : 'food_sodium'
+    'name' : 'exercise_name',
+    'description' : 'exercise_desc',
+    'primary' : 'exercise_category_primary',
+    'secondary' : 'exercise_category_secondary',
+    'img' : 'exercise_img',
+
 }, inplace =True)
 
 df.to_sql(
-    name='FoodDB',
+    name='ExerciseDB',
     con = engine,
     index = False,
     if_exists = 'append'
